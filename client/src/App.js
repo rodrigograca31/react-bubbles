0086b5ba16f294e976055a71691408ef02344c6e
rodrigograca31 <rodrigograca1993@gmail.com>
2019-12-20 08:38:19 +0000

    Stage 1 - Authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,32 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
 
 function App() {
 	return (
 		<Router>
 			<div className="App">
 				<Route exact path="/" component={Login} />
-				{/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+				<PrivateRoute exact path="/bubblepage" component={BubblePage} />
 			</div>
 		</Router>
 	);
 }
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+	<Route
+		{...rest}
+		render={props =>
+			localStorage.getItem("token") ? (
+				<Component {...props} />
+			) : (
+				<Redirect to="/" />
+			)
+		}
+	/>
+);
 
 export default App;
